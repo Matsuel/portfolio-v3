ceea4f3ae81ba3180f32fa0cc2f6309356a203d8
Matsuel <matsuel0308@gmail.com>
2024-01-14 13:11:25 +0100

    create home and add bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import topGradient from './assets/top-gradient.svg'
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <img src={topGradient} alt="top gradient" className="topGradient" />
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css"
+import RondBg from '../../assets/rondBg.svg'
+
+const Home = () => {
+    return (
+        <div className='homeWrap' id='home'>
+            <img src={RondBg} alt="rond bg" className="rondBg" />
+            
+
+        </div>
+    )
+}
+
+export default Home
