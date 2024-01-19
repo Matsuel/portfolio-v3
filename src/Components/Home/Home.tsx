76d9e114ea3a0a6d9ee045ff56d12fa7da817458
Matsuel <matsuel0308@gmail.com>
2024-01-19 19:13:15 +0100

    v1 home with contact button, log, title and little text

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import "./Home.css"
-import RondBg from '../../assets/rondBg.svg'
+import Logo from "../../assets/logo-high.png"
 
 const Home = () => {
     return (
         <div className='homeWrap' id='home'>
-            <img src={RondBg} alt="rond bg" className="rondBg" />
-            
-
+            <div className="home-content">
+                <h1 className="home-title">
+                    Hello World!
+                </h1>
+                <p className="home-text">
+                I am a full stack web developer based in France. I love back-end development and I'm always looking for new challenges like doing css.
+                </p>
+                <a className='home-button' href="#contact">Contact me</a>
+            </div>
+            <div className="logo-home">
+                <img src={Logo} alt="Logo" className='logo-image' />
+            </div>
         </div>
     )
 }
