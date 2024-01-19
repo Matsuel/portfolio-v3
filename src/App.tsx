f30ca94085e1dbe78b14bc5c1321aa2b4a6e4e97
Matsuel <matsuel0308@gmail.com>
2024-01-19 20:07:20 +0100

    creates skills components, fixed topGradient

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import topGradient from './assets/top-gradient.svg'
 import Home from './Components/Home/Home';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <img src={topGradient} alt="top gradient" className="topGradient" />
       <Navbar />
       <Home />
+      <Skills />
     </div>
   );
 }
