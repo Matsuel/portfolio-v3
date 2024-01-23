9a29be73d321c58a9dce32fdaba2b1f7612cc085
Matsuel <matsuel0308@gmail.com>
2024-01-23 09:14:13 +0100

    create conatct

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import botGradient from './assets/bot-gradient.svg'
 import Home from './Components/Home/Home';
 import Skills from './Components/Skills/Skills';
 import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Home />
       <Skills />
       <Projects />
+      <Contact />
       <img src={botGradient} alt="bot gradient" className="botGradient" />
     </div>
   );
---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <section className="contact-wrapper" id="contact">
+            
+        </section>
+    )
+}
+
+export default Contact
