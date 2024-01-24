d33122da9c11dbae97870cb4714d89299b1d47bb
Matsuel <matsuel0308@gmail.com>
2024-01-24 09:23:46 +0100

    create contact component

---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <section className="contact-container" id="contact">
+            <h1 className="contact-title">
+                Contact
+            </h1>
+            <div className="contact-form">
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Contact
