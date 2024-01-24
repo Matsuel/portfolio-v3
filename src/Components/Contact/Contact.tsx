d4328be9288f0ef89eb4d2b7916d36ac33c2e481
Matsuel <matsuel0308@gmail.com>
2024-01-24 09:38:58 +0100

    contact component work

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Contact.css'
+import MacOS from '../../assets/macOs.svg'
 
 const Contact = () => {
     return (
         <section className="contact-container" id="contact">
             <h1 className="contact-title">Contact</h1>
             <div className="contact-form">
+                <div className="line">
+                    <img src={MacOS} alt="macOS" className="macOS" />
+                    <h2 className="message-title">New Message</h2>
+                </div>
+                <div className="line">
+                    <label className='label'>Name</label>
+                    <input type="text" placeholder="Name" className="contact-input" />
+                </div>
+                <div className="line">
+                    <label className='label'>Email</label>
+                    <input type="email" placeholder="Email" className="contact-input" />
+                </div>
+                <div className="message-line">
+                    <textarea placeholder="Message" className="contact-input-message" />
+                </div>
+                
+
 
             </div>
 
