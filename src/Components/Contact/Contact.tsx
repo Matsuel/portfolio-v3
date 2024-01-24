189a2e2200a075d4167c7e0fdf11e074b984eb6e
Matsuel <matsuel0308@gmail.com>
2024-01-24 14:49:55 +0100

    dark mode for contact section

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Contact.css'
 import MacOS from '../../assets/macOs.svg'
+import { DarkModeContext } from '../../darkMode'
 
 const Contact = () => {
+    const { darkMode } = useContext(DarkModeContext)
     return (
-        <section className="contact-container" id="contact">
-            <h1 className="contact-title">Contact</h1>
-            <form className="contact-form" action='/'>
+        <section className={`contact-container ${darkMode ? 'homeWrap-dark' : 'homeWrap-light'}`} id="contact">
+            <h1 className={`contact-title ${darkMode ? 'title-light' : 'title-dark'}`}>Contact</h1>
+            <form className={`contact-form ${darkMode ? 'contact-form-dark' : 'contact-form-light'}`} action='/'>
                 <div className="line">
                     <img src={MacOS} alt="macOS" className="macOS" />
-                    <h2 className="message-title">New Message</h2>
+                    <h2 className={`message-title ${darkMode ? 'title-light' : 'title-dark'}`}>New Message</h2>
                 </div>
                 <div className="line">
-                    <label className='label'>Name:</label>
-                    <input type="text" placeholder="Enter your name please" className="contact-input" />
+                    <label className={`label ${darkMode ? 'title-light' : 'title-dark'}`}>Name:</label>
+                    <input type="text" placeholder="Enter your name please" className={`contact-input ${darkMode ? 'title-light' : 'title-dark'}`} />
                 </div>
                 <div className="line">
-                    <label className='label'>Email:</label>
-                    <input type="email" placeholder="Enter your email please" className="contact-input" />
+                    <label className={`label ${darkMode ? 'title-light' : 'title-dark'}`}>Email:</label>
+                    <input type="email" placeholder="Enter your email please" className={`contact-input ${darkMode ? 'title-light' : 'title-dark'}`} />
                 </div>
                 <div className="message-line">
-                    <textarea placeholder="Write your message here" className="contact-input-message" />
+                    <textarea placeholder="Write your message here" className={`contact-input-message ${darkMode ? 'contact-input-message-dark' : 'contact-input-message-light'}`} />
                 </div>
                 <div className="button-line">
-                    <button className="contact-button">Send</button>
+                    <button className={`contact-button ${darkMode ? 'contact-button-dark' : 'contact-button-light'}`} type="submit"
+                    >Send</button>
                 </div>
             </form>
         </section>
